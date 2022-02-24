fae3a66a50008a882489d44c04f9cf08fa75009a
gabrieldiogo <91103962+gabrieldiogo@users.noreply.github.com>
2022-02-24 00:29:02 -0300

    Update btcalert.py

--- btcalert.py
+++ btcalert.py
@@ -8,36 +8,7 @@ import urllib.request, json, time
 import pygame
 
 
-#from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
-#from requests import Request, 
 
-
-
-
-
-#def coindesk():
-    	
- #url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
- #parameters = {
- # 'start':'1',
-  #'limit':'5000',
-  #'convert':'USD'
- #}
- #headers = {
- # 'Accepts': 'application/json',
-  #'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c',
- #}
- 
- #session = Session()
- #session.headers.update(headers)
-
- #try:
- #  response = session.get(url, params=parameters)
- #  data = json.loads(response.text)
- #  print(data)
- #except (ConnectionError, Timeout, TooManyRedirects) as e:
- #  print(e)
- #api///https://pro-api.coinmarketcap.com /v1/key/info
   
   
   
